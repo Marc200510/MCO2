acdb536bf0a75ebebd185bc0e752d8667034041d
Theo Villafranca <jairus_villafranca@dlsu.edu.ph>
2025-11-21 12:14:26 +0800

    MAIN FILE

---
+++ MCO2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+public class MCO2 {
+//undirected graphs
+    Scanner sc = new Scanner(System.in);
+
+
+
+
+
+public void runMainMenu(){
+    
+System.out.println("MAIN MENU");
+System.out.println("[1] Get Friend List");
+System.out.println("[2] Get Connection");
+System.out.println("[3] Exit");
+System.out.println("Enter your choice:");
+    String choice = sc.nextLine();
+System.out.println("Enter ID of person:");
+
+
+    switch (choice) {
+        case "1":
+            //method for get friend list
+            break;
+        case "2":
+            //method for get connection
+            break;    
+        default:
+            break;
+    }
+}
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input File Path:");
+    String filePath = sc.nextLine().trim();
+
+
+}
+
+
+} 
